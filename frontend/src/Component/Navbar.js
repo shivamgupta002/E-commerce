5b33c6ec37bfa658f454cc648431b87ea29b5ec1
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-03 18:13:05 +0530

    Frontend Setup Complete

---
+++ frontend/src/Component/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <h1 className="text-3xl font-bold text-cyan-200">Navbar</h1>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/Controller.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Component/Navbar'
+
+const Controller = () => {
+  return (
+    <>
+     <Navbar/> 
+    </>
+  )
+}
+
+export default Controller
