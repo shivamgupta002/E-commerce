2230d0019d1e0775030bd53e1054e58d35403f0b
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-11 13:24:43 +0530

    Login Complete

--- backend/Controller/authController.js
+++ backend/Controller/authController.js
@@ -1,5 +1,7 @@
 import authModel from "../Models/authModel.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken"
+
 class authController {
   static registration = async (req, res) => {
     const { name, email, phone, password } = req.body;
@@ -28,6 +30,36 @@ class authController {
         }
       } else {
         return res.status(400).json({ message: "All Field are required" });
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+  static login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      if (email && password) {
+        const isUser = await authModel.findOne({ email: email });
+        if (isUser) {
+          if (
+            email === isUser.email &&
+            (await bcryptjs.compare(password, isUser.password))
+          ) {
+            const token = jwt.sign({ userId: isUser._id }, "shivam", {
+              //secrete Key is shivam
+              expiresIn: "2d",
+            });
+            return res
+              .status(200)
+              .json({ message: "Login Successful", token, name: isUser.name });
+          } else {
+            return res.status(400).json({ message: "Invalid Credentials" });
+          }
+        } else {
+          return res.status(400).json({ message: "User Not found" });
+        }
+      } else {
+        return res.status(400).json({ message: "All Fields are required" });
       }
     } catch (e) {
       console.log(e.message);
--- backend/Routes/authRoutes.js
+++ backend/Routes/authRoutes.js
@@ -4,5 +4,6 @@ import authController from "../Controller/authController.js";
 const router = express.Router();
 
 router.post("/users/register", authController.registration);
+router.post("/users/login", authController.login);
 
 export default router;
