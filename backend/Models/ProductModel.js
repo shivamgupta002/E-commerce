9c59a1dc1e1ac74f147c0dd2158914ad9c13a113
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-19 18:07:02 +0530

    Backend Product Schema Add

---
+++ backend/Models/ProductModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  price: {
+    type: String,
+  },
+  rating: {
+    type: String,
+  },
+});
+
+const productModel = mongoose.model("product", productSchema);
+
+export default productModel;
