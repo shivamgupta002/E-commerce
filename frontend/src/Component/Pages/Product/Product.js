36f0235c1b0c92016af24ce15f4a75d1f3bf0ca5
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-21 20:14:27 +0530

    Remove extra content

--- frontend/src/Component/Pages/Product/Product.js
+++ frontend/src/Component/Pages/Product/Product.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 // import Products from "./ProductData";
-import loadingImg from '../../Image/loader.gif'
+import loadingImg from "../../Image/loader.gif";
 import "./product.css";
 import { add } from "../Redux/CartSlice";
 import { useDispatch, useSelector } from "react-redux";
@@ -13,43 +13,17 @@ const Product = () => {
   const { data: product, status } = useSelector((state) => state.product);
   useEffect(() => {
     dispatch(fetchProducts());
-  });
+  }, []);
   if (status === STATUSES.Loading) {
     return (
-
       <div className="flex justify-center mt-[27vh]">
-      <img src={loadingImg} alt="loading" />
+        <img src={loadingImg} alt="loading" />
       </div>
-      )
+    );
   }
   return (
     <>
-      {/* <div className="container">
-        {Products.map((product) => (
-          <>
-            <div className="box">
-              <div className="img">
-                <img src={product.imageSrc} alt={product.imageAlt} />
-              </div>
-              <div className="content">
-                <h3 className="text-2xl">{product.name}</h3>
-                <div className="flex">
-                  <h2 className="mx-2">
-                    <span> Price :</span>
-                  </h2>{" "}
-                  <p>{product.price}</p>
-                </div>
-                <button className="cart" onClick={() => handleAdd(product)}>
-                  ADD
-                  <ion-icon name="cart-outline" className="cart"></ion-icon>
-                </button>
-              </div>
-            </div>
-          </>
-        ))}
-      </div> */}
       <h1 className="text-4xl text-center text-gray-600 my-4">Product List</h1>
-
       <div className="container">
         {product.map((product) => (
           <>
