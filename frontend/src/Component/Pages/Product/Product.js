bef2765290fc5fc36aaf1c11ac6d738cf2c43ee5
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-25 12:22:26 +0530

    Add filter

--- frontend/src/Component/Pages/Product/Product.js
+++ frontend/src/Component/Pages/Product/Product.js
@@ -5,6 +5,7 @@ import "./product.css";
 import { add } from "../Redux/CartSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { STATUSES, fetchProducts } from "../Redux/ProductSlice";
+import ProductFilter from "./ProductFilter";
 const Product = () => {
   const dispatch = useDispatch();
   const handleAdd = (product) => {
@@ -24,33 +25,37 @@ const Product = () => {
   return (
     <>
       <h1 className="text-4xl text-center text-gray-600 my-4">Product List</h1>
-      <div className="container">
-        {product.map((product) => (
-          <>
-            <div className="box">
-              <div className="rating">
-                <i class="fa-solid fa-star"></i>
-                <span>{product.rating}</span><span>/5</span>
-              </div>
-              <div className="img">
-                <img src={product.image} alt={product.imageAlt} />
-              </div>
-              <div className="content">
-                <h3 className="text-2xl">{product.title}</h3>
-                <div className="flex">
-                  <h2 className="mx-2">
-                    <span> Price :</span>
-                  </h2>{" "}
-                  <p>&#8377; {product.price}</p>
+      <div className="flex">
+        <ProductFilter />
+        <div className="container">
+          {product.map((product) => (
+            <>
+              <div className="box">
+                <div className="rating">
+                  <i class="fa-solid fa-star"></i>
+                  <span>{product.rating}</span>
+                  <span>/5</span>
                 </div>
-                <button className="cart" onClick={() => handleAdd(product)}>
-                  ADD
-                  <ion-icon name="cart-outline" className="cart"></ion-icon>
-                </button>
+                <div className="img">
+                  <img src={product.image} alt={product.imageAlt} />
+                </div>
+                <div className="content">
+                  <h3 className="text-2xl">{product.title}</h3>
+                  <div className="flex">
+                    <h2 className="mx-2">
+                      <span> Price :</span>
+                    </h2>{" "}
+                    <p>&#8377; {product.price}</p>
+                  </div>
+                  <button className="cart" onClick={() => handleAdd(product)}>
+                    ADD
+                    <ion-icon name="cart-outline" className="cart"></ion-icon>
+                  </button>
+                </div>
               </div>
-            </div>
-          </>
-        ))}
+            </>
+          ))}
+        </div>
       </div>
     </>
   );
