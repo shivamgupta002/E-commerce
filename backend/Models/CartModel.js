7a88791294871557557eb22e510c741937d1d5ec
shivamgupta002 <shivamgupta992909@gmail.com>
2023-12-07 19:24:29 +0530

    Cart Model

---
+++ backend/Models/CartModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const CartSchema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  price: {
+    type: String,
+  },
+  rating: {
+    type: String,
+  },
+  category: {
+    type: String,
+  },
+});
+
+const CartModel = mongoose.model("product", CartSchema);
+
+export default CartModel;
